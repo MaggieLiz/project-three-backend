533f5f0e8b1935a8bf528615650b51176f5f409a
maggieliz <maggielizward@gmail.com>
2021-09-22 13:39:12 +0100

    deploy test

--- config/environment.js
+++ config/environment.js
@@ -4,4 +4,4 @@ dotenv.config()
 
 export const dbURI = process.env.DB_URI || 'mongodb://localhost/muggles'
 export const port = process.env.PORT || 4000
-export const secret = process.env.SECRET || 'shhhh its a secret'
+export const secret = process.env.SECRET || 'nosecretsonlysurprises'
